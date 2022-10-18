f5adaf7d2be20488fda46e79e5e371029ff51d69
shinta0806 <shinta.0806@gmail.com>
2022-10-18 22:10:39 +0900

    Ver 0.06 時点

--- CFileMerge2/ViewModels/MainViewModel.cs
+++ CFileMerge2/ViewModels/MainViewModel.cs
@@ -1,4 +1,7 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Diagnostics;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace CFileMerge2.ViewModels;
 
@@ -6,5 +9,17 @@ public class MainViewModel : ObservableRecipient
 {
     public MainViewModel()
     {
+        ButtonBrowseMakeClickedCommand = new RelayCommand(ButtonBrowseMakeClicked);
     }
+
+    public ICommand ButtonBrowseMakeClickedCommand
+    {
+        get;
+    }
+
+    private void ButtonBrowseMakeClicked()
+    {
+        Debug.WriteLine("ButtonBrowseMakeClicked()");
+    }
+
 }
