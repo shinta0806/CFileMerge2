ad5efc59b66afdf8629389a2ec73e9693e07d56e
shinta0806 <shinta.0806@gmail.com>
2022-10-29 14:34:12 +0900

    オリジナルバージョン。

--- CFileMerge2/ViewModels/MainPageViewModel.cs
+++ CFileMerge2/ViewModels/MainPageViewModel.cs
@@ -723,7 +723,7 @@ public class MainPageViewModel : ObservableRecipient
             if (_mergeInfo.NumProgressLines % Cfm2Constants.PROGRESS_INTERVAL == 0)
             {
                 SetProgressValue(MergeStep.ParseFile, (Double)_mergeInfo.NumProgressLines / _mergeInfo.NumTotalLines);
-#if DEBUG
+#if DEBUGz
                 Thread.Sleep(20);
 #endif
             }
---
+++ README.md
@@ -0,0 +1,8 @@
+# ちょちょいとファイル合併 2
+
+静的 HTML によるヘルプの作成をアシストする開発者向けツール。
+
+詳細についてはドキュメントを参照。
+
+- ヘルプ：CFileMerge2_JPN.html
+- ソースコードについて：CFileMerge2_Src_ReadMe_JPN.txt
