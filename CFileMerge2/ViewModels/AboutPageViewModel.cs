6f5b149deef3f5c421a52041d63a2035e82830a1
shinta0806 <shinta.0806@gmail.com>
2022-11-10 16:27:31 +0900

    Ver 1.31 バージョン情報ウィンドウの外観を改善。

--- CFileMerge2/ViewModels/AboutPageViewModel.cs
+++ CFileMerge2/ViewModels/AboutPageViewModel.cs
@@ -8,18 +8,17 @@
 // 
 // ----------------------------------------------------------------------------
 
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
+using CFileMerge2.Models.SharedMisc;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 
 namespace CFileMerge2.ViewModels;
+
 public class AboutPageViewModel : ObservableRecipient
 {
     // ====================================================================
@@ -77,6 +76,11 @@ public class AboutPageViewModel : ObservableRecipient
     private readonly WindowEx _window;
 
     /// <summary>
+    /// 前回のメイン UI の高さ
+    /// </summary>
+    private Double _prevMainUiHeight;
+
+    /// <summary>
     /// 初期化済フラグ
     /// </summary>
     private Boolean _initialized;
@@ -96,18 +100,36 @@ public class AboutPageViewModel : ObservableRecipient
         }
 
         Debug.WriteLine("AboutPageViewModel.InitializeIfNeeded()");
+        _window.Title = Cfm2Constants.APP_NAME_J + "のバージョン情報";
 
         // なぜか MainWindow.xaml で Width, Height を指定しても効かないので、ここで指定する
         // Depend: 効くようになればこのコードは不要
-        _window.Width = 500;
+        _window.Width = 600;
 
         // Height は後で MainPageViewModel により指定されるはずなので、ここでは仮指定
         // 小さいと本来の高さを測定できないため、多少大きめに指定しておく
         // Depend: Window.SizeToContent が実装されればこのコードは不要
-        _window.Height = 550;
+        _window.Height = 600;
 
         // 初期化完了
         _initialized = true;
+    }
+
+    /// <summary>
+    /// イベントハンドラー：メイン UI のサイズが変更された
+    /// Depend: Window.SizeToContent が実装されればこのコードは不要
+    /// </summary>
+    public void SizeChanged(Object sender, SizeChangedEventArgs _)
+    {
+        Double mainUiHeight = ((FrameworkElement)sender).ActualHeight + 40;
+        Debug.WriteLine("MainUiSizeChanged() mainUiHeight: " + mainUiHeight);
+        if (mainUiHeight < _prevMainUiHeight)
+        {
+            return;
+        }
+
+        _window.AppWindow.ResizeClient(new Windows.Graphics.SizeInt32(_window.AppWindow.ClientSize.Width, (Int32)mainUiHeight));
+        _prevMainUiHeight = mainUiHeight;
     }
 
 }
--- CFileMerge2/Views/MainWindow.xaml.cs
+++ CFileMerge2/Views/MainWindow.xaml.cs
@@ -5,6 +5,10 @@
 // ============================================================================
 
 // ----------------------------------------------------------------------------
+// ウィンドウでの MVVM が困難であると思われるので、ウィンドウへの操作はページのビューモデルで行う
+// ----------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------
 // ToDo: 環境設定、メイクファイル履歴、文字コード・改行コード保全、
 // コマンドライン起動、関連付け、id 属性なし警告、状況依存ヘルプ用ファイル、状況依存ヘルプ実装
 // 集約イベントハンドラー、起動中はアンカーファイルの更新が反映されない？、バージョン情報
@@ -28,75 +32,5 @@ public sealed partial class MainWindow : WindowEx
         // 初期化
         AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/WindowIcon.ico"));
         Content = null;
-
-        // イベントハンドラー
-        //Activated += MainWindow_Activated;
-        //Closed += MainWindow_Closed;
     }
-
-    // ====================================================================
-    // private 変数
-    // ====================================================================
-
-    /// <summary>
-    /// 初期化済フラグ
-    /// </summary>
-    //private Boolean _initialized;
-
-    // ====================================================================
-    // private 関数
-    // ====================================================================
-
-#if false
-    /// <summary>
-    /// 必要に応じて初期化
-    /// </summary>
-    private void InitializeIfNeeded()
-    {
-        if (_initialized)
-        {
-            return;
-        }
-
-        Debug.WriteLine("InitializeIfNeeded()");
-#if DEBUG
-        Title = "［デバッグ］" + Title;
-#endif
-#if TEST
-        Title = "［テスト］" + Title;
-#endif
-
-        // なぜか MainWindow.xaml で Width, Height を指定しても効かないので、ここで指定する
-        // Depend: 効くようになればこのコードは不要
-        Width = 800;
-
-        // Height は後で MainPageViewModel により指定されるはずなので、ここでは仮指定
-        // 小さいと本来の高さを測定できないため、多少大きめに指定しておく
-        // Depend: Window.SizeToContent が実装されればこのコードは不要
-        Height = 200;
-
-        // 初期化完了
-        _initialized = true;
-    }
-
-    /// <summary>
-    /// イベントハンドラー：Activated / Deactivated
-    /// </summary>
-    private void MainWindow_Activated(Object _, WindowActivatedEventArgs args)
-    {
-        if (args.WindowActivationState == WindowActivationState.CodeActivated)
-        {
-            InitializeIfNeeded();
-        }
-    }
-
-    /// <summary>
-    /// イベントハンドラー：ウィンドウクローズ
-    /// 非 MSIX パッケージ時は実行されない、もしくは、実行が完了しないようだ
-    /// </summary>
-    private void MainWindow_Closed(object sender, WindowEventArgs args)
-    {
-        Log.Information("終了しました：" + Cfm2Constants.APP_NAME_J + " " + Cfm2Constants.APP_VER + " --------------------");
-    }
-#endif
 }
