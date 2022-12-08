c0b2781183f44eadc6da6b6309e39a4c566e4f3e
shinta0806 <shinta.0806@gmail.com>
2022-12-08 14:07:37 +0900

    Ver 2.24 環境設定ウィンドウ：ヘルプ処理に関する内部変更。

--- CFileMerge2/Views/Cfm2SettingsWindows/Cfm2SettingsWindow.xaml.cs
+++ CFileMerge2/Views/Cfm2SettingsWindows/Cfm2SettingsWindow.xaml.cs
@@ -8,31 +8,24 @@
 // ウィンドウでの MVVM が困難であると思われるので、ウィンドウへの操作はページのビューモデルで行う
 // ----------------------------------------------------------------------------
 
-using CFileMerge2.Models.SharedMisc;
-using Microsoft.UI.Windowing;
-using WinRT.Interop;
-using PInvoke;
-using System.Runtime.InteropServices;
 using System.Diagnostics;
-using Serilog.Events;
+
+using CFileMerge2.Models.SharedMisc;
+
+using Microsoft.UI.Windowing;
+
+using PInvoke;
+
 using Serilog;
-using System.Reflection.Metadata;
-using Microsoft.UI.Xaml;
+using Serilog.Events;
+using Shinta;
+using Shinta.WinUi3;
+using WinRT.Interop;
 
 namespace CFileMerge2.Views.Cfm2SettingsWindows;
 
-public delegate IntPtr Subclassproc(IntPtr hwnd, UInt32 msg, IntPtr wpalam, IntPtr lParam, IntPtr uIdSubclass, IntPtr dwRefData);
-
-
 public sealed partial class Cfm2SettingsWindow : WindowEx2
 {
-    [DllImport("Comctl32.dll")]
-    internal static extern Boolean SetWindowSubclass(IntPtr hwnd, Subclassproc subclassproc, IntPtr uIdSubclass, IntPtr dwRefData);
-
-    [DllImport("Comctl32.dll")]
-    internal static extern IntPtr DefSubclassProc(IntPtr hwnd, UInt32 msg, IntPtr wpalam, IntPtr lParam);
-
-
     // ====================================================================
     // コンストラクター
     // ====================================================================
@@ -48,10 +41,7 @@ public sealed partial class Cfm2SettingsWindow : WindowEx2
         SizeToContent = SizeToContent.Height;
         AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, Cfm2Constants.CONTENT_PATH_ICON));
         Content = new Cfm2SettingsPage(this);
-        IntPtr handle = WindowNative.GetWindowHandle(this);
-        User32.SetWindowLongFlags exStyle = (User32.SetWindowLongFlags)User32.GetWindowLong(handle, User32.WindowLongIndexFlags.GWL_EXSTYLE);
-        User32.SetWindowLong(handle, User32.WindowLongIndexFlags.GWL_EXSTYLE, exStyle | User32.SetWindowLongFlags.WS_EX_CONTEXTHELP);
-        SetWindowSubclass(handle, MySubclassproc, IntPtr.Zero, IntPtr.Zero);
+        WinUi3Common.EnableContextHelp(this, SubclassProc);
 #if false
         var a = PresenterKind;
         var b = (OverlappedPresenter)Presenter;
@@ -59,17 +49,29 @@ public sealed partial class Cfm2SettingsWindow : WindowEx2
 #endif
     }
 
-    private IntPtr MySubclassproc(IntPtr hwnd, UInt32 msg, IntPtr wpalam, IntPtr lparam, IntPtr uIdSubclass, IntPtr dwRefData)
+    // ====================================================================
+    // private 関数
+    // ====================================================================
+
+    /// <summary>
+    /// ウィンドウメッセージ処理
+    /// </summary>
+    /// <param name="hwnd"></param>
+    /// <param name="msg"></param>
+    /// <param name="wPalam"></param>
+    /// <param name="lParam"></param>
+    /// <param name="_1"></param>
+    /// <param name="_2"></param>
+    /// <returns></returns>
+    private IntPtr SubclassProc(IntPtr hwnd, UInt32 msg, IntPtr wPalam, IntPtr lParam, IntPtr _1, IntPtr _2)
     {
-        Debug.WriteLine("MySubclassproc()"+Environment.TickCount.ToString("#,0"));
-        switch (msg)
+        switch ((User32.WindowMessage)msg)
         {
-            case 0x0112:
-                Debug.WriteLine("MySubclassproc() WM_SYSCOMMAND");
-                if((UInt32)wpalam== 0xF180)
+            case User32.WindowMessage.WM_SYSCOMMAND:
+                if ((User32.SysCommands)wPalam == User32.SysCommands.SC_CONTEXTHELP)
                 {
-                    Debug.WriteLine("MySubclassproc() WM_SYSCOMMAND - SC_CONTEXTHELP");
-                    _ = Task.Run(async () => {
+                    _ = Task.Run(async () =>
+                    {
                         try
                         {
                             await Cfm2Common.ShowHelpAsync();
@@ -82,9 +84,9 @@ public sealed partial class Cfm2SettingsWindow : WindowEx2
                     });
                     return IntPtr.Zero;
                 }
-                return DefSubclassProc(hwnd, msg, wpalam, lparam);
+                return WindowsApi.DefSubclassProc(hwnd, msg, wPalam, lParam);
             default:
-                return DefSubclassProc(hwnd, msg, wpalam, lparam);
+                return WindowsApi.DefSubclassProc(hwnd, msg, wPalam, lParam);
         }
     }
 }
