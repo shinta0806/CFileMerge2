1c607e2ca7bf76b56dc89983dd9d7861188ccdcb
shinta0806 <shinta.0806@gmail.com>
2024-06-25 22:23:42 +0900

    Ver 4.03 全般：SHINTA 共通 C# ライブラリーを最新版に更新。

--- CFileMerge2/Views/Cfm2SettingsWindows/Cfm2SettingsWindow.xaml.cs
+++ CFileMerge2/Views/Cfm2SettingsWindows/Cfm2SettingsWindow.xaml.cs
@@ -15,10 +15,6 @@ using Microsoft.UI.Windowing;
 
 using Shinta.WinUi3;
 
-using Windows.Win32;
-using Windows.Win32.Foundation;
-using Windows.Win32.UI.Shell;
-
 namespace CFileMerge2.Views.Cfm2SettingsWindows;
 
 public sealed partial class Cfm2SettingsWindow : WindowEx3
@@ -34,65 +30,15 @@ public sealed partial class Cfm2SettingsWindow : WindowEx3
 	{
 		InitializeComponent();
 
+		// ヘルプボタン
+		HelpButtonParameter = "Kankyousettei";
+		IsHelpButtonEnabled = true;
+
 		// 初期化
 		SizeToContent = SizeToContent.Height;
 		AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, Cfm2Constants.CONTENT_PATH_ICON));
 		Title = Localize.Cfm2SettingsWindow_Title.Localized();
 		Content = new Cfm2SettingsPage(this);
-		_newSubclassProc = new SUBCLASSPROC(SubclassProc);
-		WinUi3Common.EnableContextHelp(this, _newSubclassProc);
-	}
-
-	// ====================================================================
-	// private 変数
-	// ====================================================================
-
-	/// <summary>
-	/// ウィンドウプロシージャー
-	/// </summary>
-	private readonly SUBCLASSPROC _newSubclassProc;
-
-	// ====================================================================
-	// private 関数
-	// ====================================================================
-
-	/// <summary>
-	/// ウィンドウメッセージ処理
-	/// </summary>
-	/// <param name="hwnd"></param>
-	/// <param name="msg"></param>
-	/// <param name="wPalam"></param>
-	/// <param name="lParam"></param>
-	/// <param name="_1"></param>
-	/// <param name="_2"></param>
-	/// <returns></returns>
-	private LRESULT SubclassProc(HWND hWnd, UInt32 msg, WPARAM wPalam, LPARAM lParam, UIntPtr _1, UIntPtr _2)
-	{
-		switch (msg)
-		{
-			case PInvoke.WM_SYSCOMMAND:
-				if ((UInt32)wPalam == PInvoke.SC_CONTEXTHELP)
-				{
-					_ = Task.Run(async () =>
-					{
-						try
-						{
-							await Cfm2Common.ShowHelpAsync(this, "Kankyousettei");
-						}
-						catch (Exception ex)
-						{
-							await ShowExceptionLogMessageDialogAsync("ヘルプ表示時エラー", ex);
-						}
-					});
-					return (LRESULT)IntPtr.Zero;
-				}
-
-				// ヘルプボタン以外は次のハンドラーにお任せ
-				return PInvoke.DefSubclassProc(hWnd, msg, wPalam, lParam);
-			default:
-				// WM_SYSCOMMAND 以外は次のハンドラーにお任せ
-				return PInvoke.DefSubclassProc(hWnd, msg, wPalam, lParam);
-		}
 	}
 }
 
--- CFileMerge2/Views/WindowEx3.cs
+++ CFileMerge2/Views/WindowEx3.cs
@@ -20,44 +20,70 @@ namespace CFileMerge2.Views;
 
 public class WindowEx3 : WindowEx2
 {
-    // ====================================================================
-    // コンストラクター
-    // ====================================================================
+	// ====================================================================
+	// コンストラクター
+	// ====================================================================
 
-    /// <summary>
-    /// メインコンストラクター
-    /// </summary>
-    public WindowEx3()
-    {
-        // コマンド
-        HelpClickedCommand = new RelayCommand<String>(HelpClicked);
-    }
+	/// <summary>
+	/// メインコンストラクター
+	/// </summary>
+	public WindowEx3()
+	{
+		// コマンド
+		HelpClickedCommand = new RelayCommand<String>(HelpClicked);
+	}
 
-    // ====================================================================
-    // public プロパティー
-    // ====================================================================
+	// ====================================================================
+	// public プロパティー
+	// ====================================================================
 
-    // --------------------------------------------------------------------
-    // コマンド
-    // --------------------------------------------------------------------
+	// --------------------------------------------------------------------
+	// コマンド
+	// --------------------------------------------------------------------
 
-    #region ヘルプリンクの制御
-    public RelayCommand<String> HelpClickedCommand
-    {
-        get;
-    }
+	#region ヘルプリンクの制御
+	public RelayCommand<String> HelpClickedCommand
+	{
+		get;
+	}
 
-    private async void HelpClicked(String? parameter)
-    {
-        try
-        {
-            await Cfm2Common.ShowHelpAsync(this, parameter);
-        }
-        catch (Exception ex)
-        {
-            await ShowLogMessageDialogAsync(LogEventLevel.Error, "ヘルプ表示時エラー：\n" + ex.Message);
-            Log.Information("スタックトレース：\n" + ex.StackTrace);
-        }
-    }
-    #endregion
+	private async void HelpClicked(String? parameter)
+	{
+		await ShowHelpAsync(parameter);
+	}
+	#endregion
+
+	// ====================================================================
+	// protected 関数
+	// ====================================================================
+
+	/// <summary>
+	/// ヘルプを表示
+	/// </summary>
+	/// <returns></returns>
+	protected async override void ShowHelp()
+	{
+		await ShowHelpAsync(HelpButtonParameter);
+	}
+
+	// ====================================================================
+	// private 関数
+	// ====================================================================
+
+	/// <summary>
+	/// ヘルプを表示
+	/// </summary>
+	/// <param name="parameter"></param>
+	/// <returns></returns>
+	private async Task ShowHelpAsync(String? parameter)
+	{
+		try
+		{
+			await Cfm2Common.ShowHelpAsync(this, parameter);
+		}
+		catch (Exception ex)
+		{
+			await ShowExceptionLogContentDialogAsync("ヘルプ表示時エラー", ex);
+		}
+	}
 }
